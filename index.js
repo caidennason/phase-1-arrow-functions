b254048e8b1ddef34582ab64681e41a4e94f2c24
caiden nason <caidenbnason@gmail.com>
2022-04-16 17:55:02 -0400

    commit tree 6227408c04fa95dd473f5adfa4943255896089b6

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function(num1 = 2000,  num2 = 100){
+    return (num1 / num2);
+}
+divide
+
+const square = (parameter) => parameter * parameter
+
+const add = (num1, num2) => num1 + num2
